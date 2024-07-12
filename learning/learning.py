a7d47c1d01dba7628f5f6b4b74c7227b065b2134
nathannewyen <nathan@nathannewyen.com>
2024-07-12 01:31:21 -0400

    add

--- learning/learning.py
+++ learning/learning.py
@@ -1,3 +1,27 @@
-print('hello world');
+import numpy as np
 
+# Create a 3-dimensional array (2x3x4)
 
+array = np.array(
+    [[
+        [[ 0,  1,  2,  3],
+         [ 4,  5,  6,  7],
+         [ 8,  9, 10, 11]],
+
+        [[12, 13, 14, 15],
+         [16, 17, 18, 19],
+         [20, 21, 22, 23]]
+    ], [
+        [[ 9,  1,  2,  3],
+         [ 8,  5,  6,  7],
+         [ 3,  9, 10, 11]],
+
+        [[1, 3, 14, 15],
+         [16, 2, 18, 19],
+         [20, 9, 22, 23]]
+    ]]
+)
+
+print(array.shape)
+selected_elements = array[:, :, 0, 1]
+print(selected_elements)
---
+++ learning/torch_code.py
@@ -0,0 +1,6 @@
+import torch
+from torch import nn
+import matplotib.pyplot as plt
+
+# Check version of PyTorch
+torch.__version__
