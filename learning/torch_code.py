a30dbbe9e72dbe08c81c4cc0945a0426871f0047
nathannewyen <nathan@nathannewyen.com>
2024-07-13 01:37:04 -0400

    update

--- learning/torch_code.py
+++ learning/torch_code.py
@@ -5,4 +5,15 @@ import matplotib.pyplot as plt
 # Check version of PyTorch
 torch.__version__
 
-# Data
+# Create *known* parameters
+weight = 0.7
+bias = 0.3
+
+# Create
+start = 0
+end = 1
+step = 0.02
+x = torch.arange(start, end, step)
+y = weight * x + bias
+
+print(x[:10], y[:10])
